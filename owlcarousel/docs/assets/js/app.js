abb170b5042227e1342ae627585aa95fc9fe1670
Juan Lima <juan_jplhotmail.com>
2023-10-05 20:30:33 -0300

    update index 1.0

--- owlcarousel/docs/assets/js/app.js
+++ owlcarousel/docs/assets/js/app.js
@@ -31,19 +31,18 @@ $(document).ready(function(){
 hljs.configure({tabReplace: '  '});
 hljs.initHighlightingOnLoad();
 
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
     loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+    margin:30,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[500])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
 })
